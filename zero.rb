b0ef20926155e4f0433460631ffa5bcea158af78
Duy Bui-Cong <duybc@pc-380.local.elarion.com>
2018-05-15 11:07:11 +0700

    Update file zero.rb - dieukien

--- zero.rb
+++ zero.rb
@@ -14,9 +14,37 @@ class Hocsinh
 	@bientrongham = 40
       puts "Xin chao #@hotena #@nna - #$bientoancuc - #@@bientrongclass - #{@bientrongham}"
    end
+
+   def chuoi
+   	arr = ["Apple","DUY","Test"]
+   	arr.each do |i| puts i end
+
+	#in tu 1 den 10
+	(1..10).each do |n| print n, ' ' end
+	#in tu 1 den 9
+	print "\n"
+	(1...10).each do |n| print n, ' ' end
+   end 
+
+   def dieukien
+	print "\n"
+	x = 10
+	if x > 20
+		print "x > 20"
+	elsif
+		print "x < 20"
+	end
+
+   end
+
 end
+
+
 
 hs1 = Hocsinh.new("DUY","Ruby")
 
 hs1.hienthi()
 hs1.hienthi2()
+hs1.chuoi()
+hs1.dieukien()
+
